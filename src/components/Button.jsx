640f3fcaa1c55c27f8ac72377c22aa92ec0faae8
ariwibi <ari.bowo.0231@gmail.com>
2023-12-04 23:49:53 +0800

    modify function buttonStyle to variants with api

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
-import { cn } from "../utils/cns";
-function buttonStyle({ color, size }) {
-  const base = "shadow-xl  hover:shadow-none font-semibold rounded";
-  const variants = {
+import { variant } from "../utils/variant";
+
+const buttonStyle = variant(
+  "shadow-xl  hover:shadow-none font-semibold rounded",
+  {
     color: {
       primary: "bg-blue-500 hover:bg-blue-600 text-white shadow-blue-300",
       danger: "bg-red-500 hover:bg-red-600 text-white shadow-red-300",
@@ -11,12 +12,14 @@ function buttonStyle({ color, size }) {
       small: "py-1 px-3",
       medium: "py-2 px-4",
     },
-  };
-
-  return cn(base, variants.color[color], variants.size[size]);
-}
+  }
+);
 export default function Button({ children, color, size }) {
-  return <button className={buttonStyle({ color, size })}>{children}</button>;
+  return (
+    <button className={buttonStyle({ color, size, corner: "rounded" })}>
+      {children}
+    </button>
+  );
 }
 
 Button.defaultProps = {
