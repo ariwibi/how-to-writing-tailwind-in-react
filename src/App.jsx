5b5c923dab149e661d72f105f9c0bc05062e17ff
ariwibi <ari.bowo.0231@gmail.com>
2023-12-04 22:34:43 +0800

    add feature varian(colo and size)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <Button>Button</Button>
-      <Button color="danger">Button</Button>
+      <Button color="danger" size="small">
+        Button
+      </Button>
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,25 @@
 /* eslint-disable react/prop-types */
 import { cn } from "../utils/cns";
 
-export default function Button({ children, color }) {
-  const colors = {
-    primary: "bg-blue-500 hover:bg-blue-600 text-white shadow-blue-300",
-    danger: "bg-red-500 hover:bg-red-600 text-white shadow-red-300",
+export default function Button({ children, color, size }) {
+  const variants = {
+    color: {
+      primary: "bg-blue-500 hover:bg-blue-600 text-white shadow-blue-300",
+      danger: "bg-red-500 hover:bg-red-600 text-white shadow-red-300",
+    },
+    size: {
+      small: "py-1 px-3",
+      medium: "py-2 px-4",
+    },
   };
   return (
     <button
       className={cn(
-        "py-2 px-4",
         "shadow-xl  hover:shadow-none",
         "font-semibold",
         "rounded",
-        colors[color]
+        variants.color[color],
+        variants.size[size]
       )}>
       {children}
     </button>
@@ -22,4 +28,5 @@ export default function Button({ children, color }) {
 
 Button.defaultProps = {
   color: "primary",
+  size: "medium",
 };
