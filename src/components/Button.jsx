6441fb840e64e34a829015b36652676afc790862
ariwibi <ari.bowo.0231@gmail.com>
2023-12-05 00:02:29 +0800

    using variant api cva

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 import { variant } from "../utils/variant";
+import { cva } from "class-variance-authority";
 
-const buttonStyle = variant(
-  "shadow-xl  hover:shadow-none font-semibold rounded",
+const buttonStyle = cva(
+  ["shadow-xl  hover:shadow-none font-semibold rounded"],
   {
-    color: {
-      primary: "bg-blue-500 hover:bg-blue-600 text-white shadow-blue-300",
-      danger: "bg-red-500 hover:bg-red-600 text-white shadow-red-300",
+    variants: {
+      color: {
+        primary: "bg-blue-500 hover:bg-blue-600 text-white shadow-blue-300",
+        danger: "bg-red-500 hover:bg-red-600 text-white shadow-red-300",
+      },
+      size: {
+        small: "py-1 px-3",
+        medium: "py-2 px-4",
+      },
     },
-    size: {
-      small: "py-1 px-3",
-      medium: "py-2 px-4",
+    defaultVariants: {
+      color: "primary",
+      size: "medium",
     },
   }
 );
@@ -21,8 +28,3 @@ export default function Button({ children, color, size }) {
     </button>
   );
 }
-
-Button.defaultProps = {
-  color: "primary",
-  size: "medium",
-};
